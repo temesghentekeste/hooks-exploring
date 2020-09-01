216af2909516307d5d3b722f06ce3b09af53d460
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-09-01 23:19:12 +0300

    useState based on previous value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-     <HookCounter/>
+      <HookCounterTwo/>
+     {/* <HookCounter/> */}
     </div>
   );
 }
---
+++ src/components/HookCounterTwo.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+function HookCounterTwo() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prevCount => prevCount + 1);    
+    }
+  }
+  return (
+    <div>
+      Count: {count}
+      <button onClick = {() => setCount(initialCount)}>Reset</button>
+      <button onClick = { () => setCount(count => ++count)}>Increment</button>
+      <button onClick = {()=>setCount(count => count - 1)}>Decrement</button>
+      <button onClick = {incrementFive}>Increment by 5</button>
+    </div>
+  )
+}
+
+export default HookCounterTwo
+
